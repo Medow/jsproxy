16150eeeaa0a8f308105d1aed39dd88358434485
Medow <baharmahir@yahoo.com>
2019-11-11 15:16:48 +0200

    Update conf.js

--- conf.js
+++ conf.js
@@ -19,7 +19,7 @@ jsproxy_config({
       hidden: false,
       lines: {
         // 收费版（高权重）
-        'cdn.testzone.gq': 4,
+        'cdn.testzone.gq': 1,
 
         // 免费版（低权重，分摊一些成本）
         // 每个账号每天 10 万次免费请求，但有频率限制
